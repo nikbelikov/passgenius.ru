7616abaa258736422a37acbed272bf9830ec0e06
nikbelikov <nikbelikov@me.com>
2013-04-08 09:43:34 +0300

    дополнения в js

--- js/init.js
+++ js/init.js
@@ -17,18 +17,18 @@ var length = 10;
 var env = $$.environment();	// environment (browser, isMobile ect.)
 
 initBrowserCheck = function(){
-	if ((!env.isMobile) || (env.isMobile && env.os.name != 'ios')){
+	if ((!env.isMobile) || (env.isMobile && env.os.name !== 'ios')){
 		$('.wrapper, #footer').remove();
 		$('body').addClass('desktop').prepend("<div id='desktop'><img src='img/desktop.png'><p>Зайдите на этот сайт с вашего iPhone и добавьте приложение на рабочий стол.</p><p id='desktop-p-img'><img class='img' src='img/icon-pg.png' alt='passgenius'><img class='img' src='img/icon-qr.png' alt='passgenius' width='79' height='79'></p><p id='footer'>Разработка: <a href='http://nikbelikov.ru/' target='_blank'>nikbelikov.ru</a></p><p id='social'><a id='git' href='https://github.com/nikbelikov/passgenius.ru' target='_blank'></a><a id='twitter' href='https://twitter.com/_nikbelikov' target='_blank'></a><a id='mail' href='mailto:nikbelikov@me.com'></a></p></div>");
 	}
-}
+};
 
 initListBtn = function(){
 	$$('.list-btn .btn').tap(function(){
 		$(this).parent().parent().find('.btn').removeClass('active');
 		$(this).addClass('active');
 	});
-}
+};
 
 initMainTabs = function(){
 	$$('#header .list-btn li').tap(function(){
@@ -37,13 +37,13 @@ initMainTabs = function(){
 		$content.removeClass('active');
 		$content.eq(ind).addClass('active');
 	});
-}
+};
 
 initInnerElements = function(){
 	$$('.dashed').tap(function(){
 		$(this).parent().next('.inner').slideToggle();
 	});
-}
+};
 
 initChangeType = function(){
 	$$('#hard').tap(function(){
@@ -61,7 +61,7 @@ initChangeType = function(){
 		chars = '1234567890';
 		length = 4;
 	});
-}
+};
 
 // function generates password
 function GeneratePass(chars,length){
@@ -81,8 +81,8 @@ function GeneratePass(chars,length){
 initGeneratePass = function(){
 	$$('#generate-btn .btn').tap(function(){
 		var compl = $('#compl').find('.btn.active').parent().index();
-		if (compl == 0) { $('.password').addClass('small'); }
+		if (compl === 0) { $('.password').addClass('small'); }
 		else { $('.password').removeClass('small'); }
 		$('.password').html(GeneratePass(chars, length)).hide().fadeIn();
 	});
-}
+};
