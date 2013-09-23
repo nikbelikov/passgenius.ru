d11e14c641c449ccb5e79ae5ee19d56c42811ca0
nikbelikov <nikbelikov@me.com>
2013-09-23 22:43:09 +0400

    v2.0 presentation link

--- v2.0/index.php
+++ v2.0/index.php
@@ -89,7 +89,7 @@
 			<a href="https://github.com/nikbelikov/passgenius.ru" target="_blank"><img src="img/icon-git.png" alt="github"></a>
 			<a href="https://twitter.com/_nikbelikov" target="_blank"><img src="img/icon-twitter.png" alt="twitter"></a>
 			<a href="mailto:nikbelikov@me.com"><img src="img/icon-mail.png" alt="mail"></a>
-			<a href="http://passgenius.ru/presentation/" target="_blank"><img src="img/icon-presentation.png" alt="presentation"></a>
+			<a href="http://passgenius.ru/v2.0/presentation/" target="_blank"><img src="img/icon-presentation.png" alt="presentation"></a>
 		</p>
 
 		<p><?php echo date('Y'); ?> (с)<br>
--- v2.0/js/init.js
+++ v2.0/js/init.js
@@ -20,7 +20,7 @@ var env = $$.environment();	// environment (browser, isMobile ect.)
 initBrowserCheck = function(){
 	if ((!env.isMobile) || (env.isMobile && env.os.name !== 'ios')){
 		$('.wrapper, #footer').remove();
-		$('body').addClass('desktop').prepend("<div id='desktop'><img src='img/desktop.png'><p>Зайдите на этот сайт с вашего iPhone и добавьте приложение на рабочий стол.</p><p id='desktop-p-img'><img class='img' src='img/icon-pg.png' alt='passgenius'><img class='img' src='img/icon-qr.png' alt='passgenius' width='79' height='79'></p><p id='footer'>Разработка: <a href='http://nikbelikov.ru/' target='_blank'>nikbelikov.ru</a></p><p id='social'><a id='github' href='https://github.com/nikbelikov/passgenius.ru' target='_blank'></a><a id='twitter' href='https://twitter.com/_nikbelikov' target='_blank'></a><a id='mail' href='mailto:nikbelikov@me.com'></a><a id='presentation' href='http://passgenius.ru/presentation/' target='_blank'></a></p></div>");
+		$('body').addClass('desktop').prepend("<div id='desktop'><img src='img/desktop.png'><p>Зайдите на этот сайт с вашего iPhone и добавьте приложение на рабочий стол.</p><p id='desktop-p-img'><img class='img' src='img/icon-pg.png' alt='passgenius'><img class='img' src='img/icon-qr.png' alt='passgenius' width='79' height='79'></p><p id='footer'>Разработка: <a href='http://nikbelikov.ru/' target='_blank'>nikbelikov.ru</a></p><p id='social'><a id='github' href='https://github.com/nikbelikov/passgenius.ru' target='_blank'></a><a id='twitter' href='https://twitter.com/_nikbelikov' target='_blank'></a><a id='mail' href='mailto:nikbelikov@me.com'></a><a id='presentation' href='http://passgenius.ru/v2.0/presentation/' target='_blank'></a></p></div>");
 	}
 };
 
