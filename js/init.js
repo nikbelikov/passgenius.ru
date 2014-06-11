8d44bc81acbdf1a6c9b15477ef9b05151a5f31e1
Nikolay Belikov <nikmail88@gmail.com>
2014-06-11 21:17:11 +0400

    localstorage
    поправил сохранение параметров пароля

--- js/init.js
+++ js/init.js
@@ -246,23 +246,26 @@ initLastPasswords = function(){
 initLocalStorage = function(){
     var getPassword = localStorage.getItem('password'),
         getLastPasswords = localStorage.getItem('lastPasswords'),
-        showLastPassIcon = localStorage.getItem('showLastPassIcon');
+        showLastPassIcon = localStorage.getItem('showLastPassIcon'),
+        settingsSymbols = localStorage.getItem('settingsSymbols'),
+        settingsNumbers = localStorage.getItem('settingsNumbers'),
+        settingsNoRepeat = localStorage.getItem('settingsNoRepeat');
 
+    // текущий пароль и список последних паролей
     if (getPassword){
-        // показываем пароль и список последних паролей
         $('.password').html(getPassword).removeClass('small');
         $('.last-passwords ul').html(getLastPasswords);
+    }
 
-        // иконка вызова блока с последними паролями
-        if (showLastPassIcon){
-            $('.last-passwords-icon').removeClass('hidden-hard');
-        }
+    // иконка вызова блока с последними паролями
+    if (showLastPassIcon){
+        $('.last-passwords-icon').removeClass('hidden-hard');
+    }
 
-        // расстановка настроек пароля (кнопки)
-        if (localStorage.getItem('settingsSymbols')){
-            $('.settings-symbols').attr('class', localStorage.getItem('settingsSymbols'));
-            $('.settings-numbers').attr('class', localStorage.getItem('settingsNumbers'));
-            $('.settings-no-repeat').attr('class', localStorage.getItem('settingsNoRepeat'));
-        }
+    // расстановка настроек пароля (кнопки)
+    if (settingsSymbols){
+        $('.settings-symbols').attr('class', settingsSymbols);
+        $('.settings-numbers').attr('class', settingsNumbers);
+        $('.settings-no-repeat').attr('class', settingsNoRepeat);
     }
 };
