212e5f51cd751c4247d1acd7006bbb762f2c259b
Nikolay Belikov <nikmail88@gmail.com>
2014-07-29 20:10:08 +0400

    скролл на десктопе

--- inc/desktop.php
+++ inc/desktop.php
@@ -6,14 +6,14 @@
         <img width="335" src="img/iphone.png" alt="iphone"/>
     </div>
 
-    <h1>Как пользоваться?</h1>
-    <p class="text-center how-to-use">
+    <h1 data-0="transform:scale(0.5)" data-600="transform:scale(1)">Как пользоваться?</h1>
+    <p class="text-center how-to-use" data-0="transform:scale(0.5)" data-800="transform:scale(1)">
         <img width="238" src="img/svg/min/homescreen.svg" alt="homescreen"/>
         <span>Зайдите на этот сайт со своего iPhone и добавьте страницу на рабочий стол.</span>
     </p>
 
-    <h1>Что нового?</h1>
-    <p class="text-center features">
+    <h1 data-0="transform:scale(0.5)" data-1000="transform:scale(1)">Что нового?</h1>
+    <p class="text-center features" data-0="transform:scale(0.3)" data-1200="transform:scale(1)">
         <img class="main-scheme" src="img/svg/min/main-scheme.svg" alt="scheme"/>
         <img width="296" height="296" class="rounded-ui" src="img/rounded-ui.png" alt="ui"/>
         <img width="68" class="icon desk-pencil" src="img/svg/min/pencil.svg" alt="pencil"/>
@@ -26,8 +26,8 @@
         <span>Работает на всех современных iPhone</span>
     </p>
 
-    <h1>Предыдущие версии</h1>
-    <div class="row versions">
+    <h1 data-1000="transform:scale(0.3)" data-1500="transform:scale(1)">Предыдущие версии</h1>
+    <div class="row versions" data-1000="transform:scale(0.5)" data-1800="transform:scale(1)">
         <div class="col-4">
             <a href="/1"><img height="198" src="img/svg/min/v1.svg" alt="scheme"/></a>
             <h2>2011</h2>
@@ -61,9 +61,9 @@
         </div>
     </div>
 
-    <div class="footer">
+    <div class="footer" data-1500="transform:scale(0.5)" data-2000="transform:scale(1)">
         <p class="text-center">
-            Специально для iPhone. <br>
+            Специально для iPhone.<br>
             Разработка: <a href="http://nikbelikov.ru/" target="_blank">nikbelikov.ru</a>
         </p>
         <p class="social">
@@ -76,9 +76,13 @@
             PassGenius.ru
         </p>
     </div>
+
+    <script src="/js/desktop/skrollr.min.js"></script>
 </div>
 
 <script>
+    var skrollr = skrollr.init();
+
     $(function(){
         $('.desk-apple').hover(function(){
             $('.main-scheme').addClass('hid');
